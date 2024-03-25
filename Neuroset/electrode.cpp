7409d8eae877e01b7c530333c8a2b62580a42330
frederickberube <frederickberube0@gmail.com>
2024-03-25 17:38:13 -0400

    Extra files

---
+++ Neuroset/electrode.cpp
@@ -0,0 +1,3 @@
+#include "electrode.h"
+
+Electrode::Electrode() {}
---
+++ Neuroset/electrode.h
@@ -0,0 +1,10 @@
+#ifndef ELECTRODE_H
+#define ELECTRODE_H
+
+class Electrode
+{
+public:
+    Electrode();
+};
+
+#endif // ELECTRODE_H
