7d670d8bd20fe161ce3690943597dff569fbf547
frederickberube <frederickberube0@gmail.com>
2024-03-25 17:10:00 -0400

    improved io

--- Neuroset/mainwindow.cpp
+++ Neuroset/mainwindow.cpp
@@ -1,23 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
-#include <QDir>
+#include <QListView>
+#include <QStringListModel>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    qDebug() << QDir::currentPath();
+
+    //Setting up UI icons
     ui->setupUi(this);
     ui->powerButton->setIcon(QIcon(":/images/images/PowerOn.png"));
     ui->stopButton->setIcon(QIcon(":/images/images/stop.png"));
     ui->playButton->setIcon(QIcon(":/images/images/play.png"));
     ui->pauseButton->setIcon(QIcon(":/images/images/pause.png"));
-
+    ui->upButton->setIcon(QIcon(":/images/images/up.png"));
+    ui->downButton->setIcon(QIcon(":/images/images/down.png"));
     QPixmap pixmap(":/images/images/4Battery.png");
-
-
     ui->battery->setPixmap(pixmap);
+
+
 }
 
 MainWindow::~MainWindow()
