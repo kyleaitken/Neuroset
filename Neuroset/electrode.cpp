2903e3170253e7e15c7479fa1e5e4ae8d0acebd8
frederickberube <frederickberube0@gmail.com>
2024-03-29 14:46:59 -0400

    today's work

--- Neuroset/electrode.cpp
+++ Neuroset/electrode.cpp
@@ -1,24 +1,34 @@
 #include "electrode.h"
 
-Electrode::Electrode(int electrodeNum) : electrodeNum(electrodeNum) {
+Electrode::Electrode(int electrodeNum, const QString &electrodeSiteName) : electrodeNum(electrodeNum)
+{
+    freqData.electrodeSite = electrodeSiteName;
 }
 
-void Electrode::getBaselineFrequency() {
+void Electrode::getInitialBaselineFrequency()
+{
     qInfo() << "Electrode " << electrodeNum << " getting baseline freq in thread: " << QThread::currentThreadId();
     QThread::msleep(2000);
+    freqData.initialFreq = 10;
     emit initialBaselineFinished(electrodeNum);
 }
 
-void Electrode::getFinalBaselineFrequency() {
+void Electrode::getFinalBaselineFrequency()
+{
     qInfo() << "Electrode " << electrodeNum << " getting baseline freq in thread: " << QThread::currentThreadId();
-    qInfo() << "intiial frequency for " << electrodeNum << " was " << initialFreq;
     QThread::msleep(2000);
+    freqData.finalFreq = 15;
     emit finalBaselineFinished(electrodeNum);
 }
 
-bool Electrode::startTreatment() {
+bool Electrode::startTreatment()
+{
     qInfo() << "Electrode " << electrodeNum << " starting treatment in thread: " << QThread::currentThreadId();
     QThread::msleep(3000);
-    initialFreq = 30;
     return true;
+}
+
+FrequencyData Electrode::getElectrodeData()
+{
+    return freqData;
 }
