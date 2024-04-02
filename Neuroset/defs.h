975eb8bb0d787002295c4a1fc96dd2ab7331a2c0
student <kyleaitken24@gmail.com>
2024-04-02 00:19:42 -0400

    defs documentation

--- Neuroset/defs.h
+++ Neuroset/defs.h
@@ -4,7 +4,7 @@
 #include <QVector>
 #include <QString>
 
-constexpr float TREATMENT_TIME_SECONDS = 1380;
+constexpr float TREATMENT_TIME_SECONDS = 1380; // 23 minutes
 extern QVector<QString> eegSiteNames;
 
 #endif
